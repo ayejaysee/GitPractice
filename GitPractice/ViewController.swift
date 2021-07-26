45bd4592344db2ebd2ad5b4c21726e9d2c779220
Andrew Chong <andrew.chong@gm.com>
2021-07-26 11:16:56 -0500

    first

---
+++ GitPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitPractice
+//
+//  Created by Andrew Chong on 7/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
